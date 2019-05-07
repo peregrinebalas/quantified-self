01398bfe6e9e5d24ab7d2df3e14b1331dc6f1290
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-05-07 16:30:05 -0600

    Updated create food migration

--- migrations/20190507204826-create-food.js
+++ migrations/20190507204826-create-food.js
@@ -9,9 +9,11 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       name: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       calories: {
+        allowNull: false,
         type: Sequelize.INTEGER
       },
       createdAt: {
