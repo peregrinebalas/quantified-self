c23d5c8a620d32d9e25124b5f4afd032ed332e1b
April Dagonese <adagonese@gmail.com>
2019-05-08 17:25:45 -0600

    Adds tests for GET call for meal

--- test/requests/meal.spec.js
+++ test/requests/meal.spec.js
@@ -5,29 +5,49 @@ var express = require('express');
 var test = express()
 var app = require('../../app.js');
 var clear = require('../specHelper')
+const Meal = require('../../models').Meal
+const User = require('../../models').User
+const Food = require('../../models').Food
+const MealFood = require('../../models').MealFood
 
 test.use(bodyParser.json())
-test.use(bodyParser.urlencoded({ extended: false }))
+test.use(bodyParser.urlencoded({ extended: true }))
 
 describe('api', () => {
   beforeAll(() => {
+    shell.exec('npx sequelize db:drop')
     shell.exec('npx sequelize db:create')
+    shell.exec('npx sequelize db:migrate')
+    shell.exec('npx sequelize db:seed:all')
   });
-});
 
-describe('Users Endpoints', () => {
-  test('POST request for new meal', () => {
-    const body = {
-                    api_key: "klasjfd3oiu05704309284fyouysgy",
-                    name: "Breakfast",
-                    date: "1/5/19"
-                  }
-    return request(app)
-    .post("/api/v1/meals")
-    .send(body)
-    .then(response => {
-      expect(response.statusCode).toBe(201)
-      expect(typeof response.body.api_key).toBe("string")
+  describe('Meals Endpoints', () => {
+    it('GET request for meal', () => {
+      const body = {
+        api_key: "1234"
+      }
+      return request(app)
+              .post("/api/v1/meals/")
+              .send(body)
+              .then(response => {
+        expect(response.body).toBe(201)
+      });
+    });
+
+    it('POST request for new meal', () => {
+      const body = {
+        api_key: "klasjfd3oiu05704309284fyouysgy",
+        meal: "Breakfast",
+        date: "1/5/19"
+      }
+      return request(app)
+      .post("/api/v1/meals/")
+      .send(body)
+      .then(response => {
+        expect(response.statusCode).toBe(201)
+        expect(response.body.id).toBe("3")
+        expect(response.body.message).toBe("Breakfast has been added to meals for 1/5/19")
+      });
     });
   });
 });
