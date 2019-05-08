c833cd63afb261f377f542dc9fb774012ddde205
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-05-07 20:50:01 -0600

    Added index for food

--- controllers/foodsController.js
+++ controllers/foodsController.js
@@ -8,7 +8,6 @@ const add = (req, res) => {
     calories: req.body.calories
   })
   .then(food => {
-    eval(pry.it)
     res.setHeader("Content-Type", "application/json");
     res.status(201).send(JSON.stringify(food));
   })
@@ -18,7 +17,15 @@ const add = (req, res) => {
   });
 }
 
-const index = (request, response) => {
+const index = async (req, res) => {
+  try {
+    const foods = await Food.findAll();
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).send(JSON.stringify(foods));
+  } catch (error) {
+    res.setHeader("Content-Type", "application/json");
+    res.status(404).send({error});
+  }
 }
 
 module.exports = {
