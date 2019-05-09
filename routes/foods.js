2b573db74ca476977d64bdcfcc17a45726195b4c
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-05-09 08:17:19 -0600

    Added route to delete food

--- routes/foods.js
+++ routes/foods.js
@@ -7,5 +7,6 @@ router.post('/', foodsController.add);
 router.get('/', foodsController.index);
 router.get('/:id', foodsController.show);
 router.put('/:id', foodsController.update);
+router.delete('/:id', foodsController.destroy);
 
 module.exports = router;
